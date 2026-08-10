ca58a53ae988fb82501a09d06e1228047d165fe4
OMPRAKSH SENTHILKUMAR <omprakash26705@gmail.com>
2026-08-10 11:26:32 +0530

    Time: 11 ms (97.89%), Space: 47.9 MB (97.54%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>(
+                (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        for (int num : nums) {
+            while (!heap.isEmpty() && heap.peek()[0] < num - 1) {
+                if (heap.poll()[1] < 3) {
+                    return false;
+                }
+            }
+            if (!heap.isEmpty() && heap.peek()[0] == num - 1) {
+                int[] top = heap.poll();
+                heap.offer(new int[] { num, top[1] + 1 });
+            } else {
+                heap.offer(new int[] { num, 1 });
+            }
+        }
+        while (!heap.isEmpty()) {
+            if (heap.poll()[1] < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
